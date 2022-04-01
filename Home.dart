224c9bdf9616e9f9691360a53fe330f0df6a55c4
marcelo-cantalice-testes <katakumbio@gmail.com>
2022-04-01 10:22:33 -0300

    segundo commit

--- Home.dart
+++ Home.dart
@@ -1,5 +1,9 @@
 //import 'dart:html';
 
+// ignore_for_file: file_names, prefer_const_constructors
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -39,17 +43,68 @@ class _HomeState extends State<Home> {
                   Container(
                     padding: EdgeInsets.all(8),
                     decoration: BoxDecoration(
-                        color: Colors.green,
+                        color: Colors.white,
                         borderRadius: BorderRadius.circular(8),
-                        boxShadow: [BoxShadow(color: Colors.grey)]),
-                    child: Text("Area login"),
+                        boxShadow: [
+                          BoxShadow(color: Colors.grey)
+                        ]), //Cria uma sombra para o Widget
+                    child: Column(
+                      //Campo do e-mail
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          child: TextField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.person),
+                                border: InputBorder.none,
+                                hintText: "Email",
+                                hintStyle: TextStyle(
+                                    color: Colors.grey[600], fontSize: 18)),
+                          ),
+                        ),
+                        Container(
+                          //campo da senha
+                          padding: EdgeInsets.all(8),
+                          child: TextField(
+                            obscureText: true,
+                            keyboardType: TextInputType.visiblePassword,
+                            decoration: InputDecoration(
+                                icon: Icon(Icons.lock),
+                                border: InputBorder.none,
+                                hintText: "Senha",
+                                hintStyle: TextStyle(
+                                    color: Colors.grey[600], fontSize: 18)),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   SizedBox(
                     //cria uma box de tamanho definido para coloocar um widget
                     height: 20,
-                  /*  child: InkWell(
+                    /*  child: InkWell(
                         //funciona como Gesture Dector, porem aplkicando sobre um obj retagular
                         ),*/
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      height: 50,
+                      child: Center(
+                        child: Text(
+                          "Entrar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(colors: [Color.fromRGBO(255, 100, 127, 1), Color.fromRGBO(255, 123, 145, 1)])
+                      ),
+                    ),
                   ),
                   SizedBox(
                     height: 10,
